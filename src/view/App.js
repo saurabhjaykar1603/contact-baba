cbfee4c857e9f774f96eeaa982e85b442aab0c6f
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-09-25 01:42:16 +0530

    contact-card

--- src/view/App.js
+++ src/view/App.js
@@ -1,14 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
+import { phonebook } from "../../data/Phonebook";
 import './App.css'
-import {phonebook} from "../components/data/phonebook"
+import ContactCard from "../../components/ContactCard/ContactCard";
+ function App() {
+   const [Contact, setContacts] = useState(phonebook);
+   const [searchterm, setsearchterm] =useState('')
+   useEffect (() => {
+   const filteredContacts =phonebook.filter((contact) =>{
+   const name = contact.name.toLocaleLowerCase();
+   const mobile = contact.mobile.toString();
+   const qury =searchterm.toLocaleLowerCase();
 
-const App = () => {
-    const [contact , setContacts ] = useState(phonebook)
-  return (
-    <>
-      <h1 className='text-center'>Contact Baba</h1>
-    </>
-  )
-}
+   return (name.includes(qury) || mobile.includes(qury))
 
-export default App
+   })
+   setContacts(filteredContacts);
+
+   } ,[searchterm])
+    return(
+        <>
+       
+       <h1 className="text-center">Contact App</h1>
+       <input type='text'placeholder='search' className="searchbar"
+        value={searchterm}
+        onChange={(e) => {setsearchterm(e.target.value)}}
+        />
+       <div>
+        {Contact.map((contact ,index) =>{
+            const {name,mobile} =contact;
+
+
+            return <ContactCard key ={index} name={name} mobile={mobile}/>
+        } )}
+       </div>
+       {
+        Contact.length === 0 ? <h2 className="text-center">No contact found</h2> : null
+       }
+       </>
+    )
+ }
+ export default App
